ad91b50f2fc0d138766beb16d6f1674881438669
Eric Wanyama <wafulawanyama@gmail.com>
2019-05-04 17:19:22 +0300

    added change in quote-details.component.html to replace quoteComplete with quoteDelete

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,6 +8,7 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let today: Date = new Date(); //get current date and time
     let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    // let todayFullDate: any = new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes(), today.getSeconds())
     var dateDifference = Math.abs(todayWithNoTime - value)// returns value in milliseconds
     const secondsInADay = 86400; //60 seconds*60 minutes in an hour *24 hours
 
