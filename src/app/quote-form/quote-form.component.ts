9f547dd23b80782abc1bb6d6dcf211a823f81984
Eric Wanyama <wafulawanyama@gmail.com>
2019-05-05 23:11:40 +0300

    updated the quote component template. added the output event addQuote that triggers the addNewQuote function passing in the new quote that is emitted.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -9,10 +9,10 @@ import {Quote} from '../quote';
 export class QuoteFormComponent implements OnInit {
 
   newQuote = new Quote(0, "", "", new Date());
-  @Output() addQuote=new EventEmitter<Quote>()
+  @Output() addQuote=new EventEmitter<Quote>();
 
   submitQuote(){
-    this.addQuote.emit(this.newQuote)
+    this.addQuote.emit(this.newQuote);
   }
 
   constructor() { }
